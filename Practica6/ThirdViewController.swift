d0881f2dfd0dd8e02f693649db1d25f1cacf418a
Carlos Moro García <carlosmoro95@gmail.com>
2016-12-17 23:36:44 +0100

    Tabla Equipos

--- Practica6/ThirdViewController.swift
+++ Practica6/ThirdViewController.swift
@@ -8,28 +8,59 @@
 
 import UIKit
 
-class ThirdViewController: UIViewController {
-
+class ThirdViewController: UITableViewController {
+    
+    var prueba = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        self.navigationItem.title = "Liga"
+        
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+        
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "Clasificación"
     }
-    */
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Equipo", for: indexPath)
+        
+        cell.imageView?.image = prueba%2 == 0 ? #imageLiteral(resourceName: "first") : #imageLiteral(resourceName: "second")
+        cell.textLabel?.text = "Nombre equipo"
+        cell.detailTextLabel?.text = "Puntuación y datos"
+        
+        prueba += 1
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let numeroEquiposLiga = 20
+        
+        return numeroEquiposLiga
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "Plantilla" {
+            if let pvc = segue.destination as? FourthViewController {
+                // IndexPath de la celda seleccionada
+                if let ip = tableView.indexPathForSelectedRow {
+                    //pvc.respuesta = "\(ip.section * ip.row)"
+                }
+            }
+        }
+    }
+    
 
 }
